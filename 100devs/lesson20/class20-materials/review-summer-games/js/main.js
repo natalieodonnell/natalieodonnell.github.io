bf7eaa832eeade45dcd94e63165aa455ad7e498a
natalieodonnell <55251799+natalieodonnell@users.noreply.github.com>
2025-05-05 14:50:26 -0400

    leon class 21

--- 100devs/lesson20/class20-materials/review-summer-games/js/main.js
+++ 100devs/lesson20/class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,9 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multiNumsInArray(arr) {
+    let product = 1
+    for (let i = 0; i < arr.length; i++) {
+        product *= arr[i];
+    }
+    alert(product)
+}
+multiNumsInArray([10, 2, 3]);
